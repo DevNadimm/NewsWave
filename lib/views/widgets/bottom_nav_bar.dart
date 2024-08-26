bf2e9dab5c8a41a00a6366f2f56df319c8732229
DevNadimm <nadimchowdhury87@gmail.com>
2024-08-26 19:33:19 +0600

    new screen create

---
+++ lib/views/widgets/bottom_nav_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:news_wave/colors.dart';
+import 'package:news_wave/views/screens/book_screen.dart';
+import 'package:news_wave/views/screens/home_screen.dart';
+import 'package:news_wave/views/screens/setting_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  final List<Widget> screens = [
+    const HomeScreen(),
+    const BookScreen(),
+    const SettingScreen(),
+  ];
+
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: IndexedStack(
+        index: currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          BottomNavigationBar(
+            backgroundColor: bgColor,
+            currentIndex: currentIndex,
+            onTap: (index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Books',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              ),
+            ],
+            selectedItemColor: accentColor,
+            unselectedItemColor: subTextColor,
+            showUnselectedLabels: true,
+            showSelectedLabels: true,
+          ),
+
+        ],
+      ),
+    );
+  }
+}
