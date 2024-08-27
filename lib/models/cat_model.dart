8cbcb5b6cc57d47cf58df15128774248d4daeb56
DevNadimm <nadimchowdhury87@gmail.com>
2024-08-27 09:35:08 +0600

    new screen create

---
+++ lib/models/cat_model.dart
@@ -0,0 +1,42 @@
+Map<String, dynamic> categoryMap = {
+  'world': {
+    'name': 'World News',
+    'imagePath': 'assets/images/world_news.jpg',
+  },
+  'technology': {
+    'name': 'Technology',
+    'imagePath': 'assets/images/technology.jpg',
+  },
+  'business': {
+    'name': 'Business',
+    'imagePath': 'assets/images/business.jpg',
+  },
+  'sports': {
+    'name': 'Sports',
+    'imagePath': 'assets/images/sports.jpg',
+  },
+  'entertainment': {
+    'name': 'Entertainment',
+    'imagePath': 'assets/images/entertainment.jpg',
+  },
+  'health': {
+    'name': 'Health',
+    'imagePath': 'assets/images/health.jpg',
+  },
+  'science': {
+    'name': 'Science',
+    'imagePath': 'assets/images/science.jpg',
+  },
+  'travel': {
+    'name': 'Travel',
+    'imagePath': 'assets/images/travel.jpg',
+  },
+  'food': {
+    'name': 'Food',
+    'imagePath': 'assets/images/food.jpg',
+  },
+  'politics': {
+    'name': 'Politics',
+    'imagePath': 'assets/images/politics.jpg',
+  },
+};
--- lib/views/screens/search_screen.dart
+++ lib/views/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_wave/colors.dart';
+import 'package:news_wave/models/cat_model.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -8,36 +9,102 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "assets/logo/news_wave_title.png",
+          width: 150,
+        ),
+        // Text(
+        //   'NewsWave',
+        //   style: Theme.of(context).textTheme.titleLarge,
+        // ),
+        centerTitle: false,
+        surfaceTintColor: Colors.transparent,
+      ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "What are you looking for?",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CupertinoSearchTextField(
-                padding: const EdgeInsets.all(10),
-                placeholder: "Search...",
-                placeholderStyle: Theme.of(context).textTheme.titleSmall,
-                backgroundColor: secondaryColor,
-                borderRadius: BorderRadius.circular(10),
-                prefixIcon: const Icon(
-                  CupertinoIcons.search,
-                  color: subTextColor,
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 10,
                 ),
-                suffixIcon: const Icon(
-                  CupertinoIcons.clear_circled,
-                  color: subTextColor,
+                Text(
+                  "What are you looking for?",
+                  style: Theme.of(context).textTheme.titleMedium,
                 ),
-                style: Theme.of(context).textTheme.titleSmall,
-              ),
-            ],
+                const SizedBox(height: 10),
+                CupertinoSearchTextField(
+                  padding: const EdgeInsets.all(10),
+                  placeholder: "Search...",
+                  placeholderStyle: Theme.of(context).textTheme.titleSmall,
+                  backgroundColor: secondaryColor,
+                  borderRadius: BorderRadius.circular(10),
+                  prefixIcon: const Icon(
+                    CupertinoIcons.search,
+                    color: subTextColor,
+                  ),
+                  suffixIcon: const Icon(
+                    CupertinoIcons.clear_circled,
+                    color: subTextColor,
+                  ),
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "Trending Categories",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const SizedBox(height: 10),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: categoryMap.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1.5,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    String key = categoryMap.keys.elementAt(index);
+                    Map<String, dynamic> category = categoryMap[key];
+
+                    return Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: AssetImage(category['imagePath']),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.center,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              category['name'],
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
