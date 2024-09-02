1384bb7b66825ebc27669e844a1cad7ee8f74c03
DevNadimm <nadimchowdhury87@gmail.com>
2024-09-02 21:13:40 +0600

    update

--- lib/views/screens/details_news_screen.dart
+++ lib/views/screens/details_news_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news_wave/colors.dart';
 import 'package:news_wave/models/news_model.dart';
 
 class DetailsNewsScreen extends StatefulWidget {
@@ -28,32 +29,118 @@ class _DetailsNewsScreenState extends State<DetailsNewsScreen> {
         return FadeTransition(opacity: animation, child: child);
       },
       child: Scaffold(
-          key: ValueKey<bool>(isBookmarked),
-          appBar: AppBar(
-            actions: [
-              IconButton(
-                onPressed: _toggleBookmark,
-                icon: Icon(
-                  isBookmarked
-                      ? CupertinoIcons.bookmark_fill
-                      : CupertinoIcons.bookmark,
-                  size: 20,
-                ),
+        key: ValueKey<bool>(isBookmarked),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: _toggleBookmark,
+              icon: Icon(
+                isBookmarked
+                    ? CupertinoIcons.bookmark_fill
+                    : CupertinoIcons.bookmark,
+                size: 20,
               ),
-            ],
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(10),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   widget.article.title,
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 16),
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(fontSize: 16),
                 ),
-
+                const SizedBox(
+                  height: 7,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 10,
+                          backgroundColor: accentColor,
+                          child: Center(
+                            child: Text(
+                              widget.article.author.isEmpty
+                                  ? "U"
+                                  : widget.article.author[0],
+                              style: const TextStyle(
+                                  color: bgColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 7,
+                        ),
+                        Text(
+                          widget.article.author.isEmpty
+                              ? "Unknown"
+                              : widget.article.author,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style:
+                              Theme.of(context).textTheme.titleSmall?.copyWith(
+                                    fontSize: 14,
+                                    color: textColor.withOpacity(0.9),
+                                  ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      _formatDate(widget.article.publishedAt),
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            fontSize: 13,
+                          ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    widget.article.urlToImage.toString(),
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                Text(
+                  widget.article.description.toString(),
+                  style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                    fontSize: 14,
+                    color: textColor.withOpacity(0.9),
+                  ),
+                ),
+                Text(
+                  widget.article.content.toString(),
+                  style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                        fontSize: 14,
+                        color: textColor.withOpacity(0.9),
+                      ),
+                )
               ],
             ),
-          )),
+          ),
+        ),
+      ),
     );
+  }
+
+  String _formatDate(DateTime? date) {
+    if (date == null) {
+      return 'Unknown date';
+    }
+    return "${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}";
   }
 }
