0727b66354d21d2c65eb5e638217c6c4e102b6bc
DevNadimm <nadimchowdhury87@gmail.com>
2024-09-07 15:49:14 +0600

    update

--- lib/views/screens/bookmark_screen.dart
+++ lib/views/screens/bookmark_screen.dart
@@ -12,6 +12,12 @@ class BookmarkScreen extends StatefulWidget {
 }
 
 class _BookmarkScreenState extends State<BookmarkScreen> {
+  Future<void> _refreshBookmarks() async {
+    setState(() {
+      Bookmark.bookmarkArticle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,111 +26,124 @@ class _BookmarkScreenState extends State<BookmarkScreen> {
           'Bookmark',
           style: Theme.of(context).textTheme.titleMedium,
         ),
+        surfaceTintColor: Colors.transparent,
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: ListView.separated(
-          itemCount: Bookmark.bookmarkArticle.length,
-          itemBuilder: (context, index) {
-            final article = Bookmark.bookmarkArticle[index];
-            final imageUrl = article.urlToImage ??
-                'https://i.ibb.co/nwgFFQf/20240901-180827.jpg';
-            final headline = article.title;
-            final authorName =
-                article.author.isNotEmpty ? article.author : 'Unknown';
-            final authorInitial =
-                article.author.isNotEmpty ? article.author[0] : "U";
-            final formattedDate = _formatDate(article.publishedAt);
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  CupertinoPageRoute(
-                    builder: (_) => DetailsNewsScreen(
-                      article: article,
-                      isBookmarked: true,
-                    ),
-                  ),
-                );
-              },
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: secondaryColor,
-                  borderRadius: BorderRadius.circular(12),
+        child: Bookmark.bookmarkArticle.isEmpty
+            ? Center(
+                child: Text(
+                  "No bookmarks available!",
+                  style: Theme.of(context).textTheme.titleSmall,
                 ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 120,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        color: bgColor,
-                        borderRadius: BorderRadius.circular(12),
-                        image: DecorationImage(
-                          image: NetworkImage(imageUrl),
-                          fit: BoxFit.cover,
+              )
+            : RefreshIndicator(
+                onRefresh: _refreshBookmarks,
+                child: ListView.separated(
+                  itemCount: Bookmark.bookmarkArticle.length,
+                  itemBuilder: (context, index) {
+                    final article = Bookmark.bookmarkArticle[index];
+                    final imageUrl = article.urlToImage ??
+                        'https://i.ibb.co/nwgFFQf/20240901-180827.jpg';
+                    final headline = article.title;
+                    final authorName =
+                        article.author.isNotEmpty ? article.author : 'Unknown';
+                    final authorInitial =
+                        article.author.isNotEmpty ? article.author[0] : "U";
+                    final formattedDate = _formatDate(article.publishedAt);
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                            builder: (_) => DetailsNewsScreen(
+                              article: article,
+                              isBookmarked: true,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: secondaryColor,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 120,
+                              width: 120,
+                              decoration: BoxDecoration(
+                                color: bgColor,
+                                borderRadius: BorderRadius.circular(12),
+                                image: DecorationImage(
+                                  image: NetworkImage(imageUrl),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    headline,
+                                    style:
+                                        Theme.of(context).textTheme.titleMedium,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Row(
+                                    children: [
+                                      CircleAvatar(
+                                        radius: 10,
+                                        backgroundColor: accentColor,
+                                        child: Text(
+                                          authorInitial,
+                                          style: const TextStyle(
+                                              color: bgColor,
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                      const SizedBox(width: 8),
+                                      Expanded(
+                                        child: Text(
+                                          authorName,
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleSmall,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    formattedDate,
+                                    style:
+                                        Theme.of(context).textTheme.bodyMedium,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            headline,
-                            style: Theme.of(context).textTheme.titleMedium,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          const SizedBox(height: 8),
-                          Row(
-                            children: [
-                              CircleAvatar(
-                                radius: 10,
-                                backgroundColor: accentColor,
-                                child: Text(
-                                  authorInitial,
-                                  style: const TextStyle(
-                                      color: bgColor,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 8,
-                              ),
-                              Expanded(
-                                child: Text(
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  authorName,
-                                  style: Theme.of(context).textTheme.titleSmall,
-                                ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            formattedDate,
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const SizedBox(
+                      height: 10,
+                    );
+                  },
                 ),
               ),
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return const SizedBox(
-              height: 10,
-            );
-          },
-        ),
       ),
     );
   }
