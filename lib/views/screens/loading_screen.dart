6014834b143b8b9edc18c531f33ac499d21768b0
DevNadimm <nadimchowdhury87@gmail.com>
2024-09-10 12:52:29 +0600

    update

--- lib/views/screens/loading_screen.dart
+++ lib/views/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_wave/utils/loading_container.dart';
+import 'package:news_wave/views/widgets/loading_container.dart';
 import 'package:news_wave/utils/colors.dart';
 
 class LoadingScreen extends StatelessWidget {
